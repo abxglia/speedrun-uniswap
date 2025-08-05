3d06f11028f51e89b1e41d691abb1996f92d7e4d
abxglia <abhiprajapati@lampros.tech>
2025-08-05 06:32:43 +0000

    initial challenge submission

--- packages/nextjs/app/debug/_components/UniswapInterface.tsx
+++ packages/nextjs/app/debug/_components/UniswapInterface.tsx
@@ -189,7 +189,7 @@ export default function UniswapInterface() {
       const privateKey = process.env.NEXT_PUBLIC_PRIVATE_KEY || '';
       const signer = new ethers.Wallet(privateKey, provider);
       const signerAddress = await signer.getAddress();
-      const uniswapContractAddress = "0x40b694c75b4e509aa8cbe9993a37af9546ce36e8";
+      const uniswapContractAddress = "0x036c64fa5af80d14d976a62e25614075ca6a0fb7";
       const uniswapContract = new ethers.Contract(uniswapContractAddress, IUniswapV2, signer);
       console.log("contract", uniswapContract);
       setContract(uniswapContract);
